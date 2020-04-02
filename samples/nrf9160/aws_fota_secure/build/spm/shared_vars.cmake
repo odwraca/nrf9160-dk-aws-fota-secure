9d2dbe2f8d9ec3bfea8434602204135da80b6b9e
aaarcr <aaarcr@amazon.com>
2020-04-02 11:04:03 -0500

    First commit

---
+++ samples/nrf9160/aws_fota_secure/build/spm/shared_vars.cmake
@@ -0,0 +1,6 @@
+set(spm_KERNEL_HEX_NAME zephyr.hex)
+set(spm_KERNEL_ELF_NAME zephyr.elf)
+list(APPEND spm_BUILD_BYPRODUCTS /Users/aaarcr/ncs/nrf/samples/nrf9160/aws_fota_secure/build/spm/zephyr/zephyr.hex)
+list(APPEND spm_BUILD_BYPRODUCTS /Users/aaarcr/ncs/nrf/samples/nrf9160/aws_fota_secure/build/spm/zephyr/zephyr.elf)
+list(APPEND spm_BUILD_BYPRODUCTS
+    /Users/aaarcr/ncs/nrf/samples/nrf9160/aws_fota_secure/build/spm/libspmsecureentries.a)
